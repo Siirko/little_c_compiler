b4ff8461720dabeee8b6aff54ac20e887e28b3ef
Siirko <alexander.yanovskyy@gmail.com>
2023-12-04 21:41:54 +0100

    added link repo to vec lib

--- src/vec.c
+++ src/vec.c
@@ -1,9 +1,4 @@
-/**
- * Copyright (c) 2014 rxi
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the MIT license. See LICENSE for details.
- */
+// https://github.com/rxi/vec
 
 #include "../include/vec.h"
 
