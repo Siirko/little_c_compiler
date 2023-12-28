032a5c53211b0e00f8c3f0a05f0adc63c279be09
Siirko <alexander.yanovskyy@gmail.com>
2023-12-28 19:34:03 +0100

    removed empty hashmaps that interferece with indexing of scopes

--- src/mips.c
+++ src/mips.c
@@ -8,18 +8,24 @@ void mips_data_section(hashmap_t *t_sym_tab, FILE *file)
 
     // we focus only on "main" function, but in the future it will be changed
     vec_vec_hashmap_t *v_scopes = (vec_vec_hashmap_t *)hashmap_get(t_sym_tab, "main");
-    int i;
-    vec_hashmap_t tmp;
-    vec_foreach(v_scopes, tmp, i)
+    vec_hashmap_t *tmp;
+    for (int i = 0; i < v_scopes->length; i++)
     {
-        if (tmp.length == 0)
+        tmp = &v_scopes->data[i];
+        if (tmp->length == 0)
             continue;
         int j;
         hashmap_t *tmp2;
-        vec_foreach(&tmp, tmp2, j)
+        vec_foreach(tmp, tmp2, j)
         {
+            // clean empty hashmaps
             if (tmp2->count == 0)
+            {
+                hashmap_free(tmp2);
+                free(tmp2);
+                vec_splice(tmp, j, 1);
                 continue;
+            }
             hashmap_iter_t iter = {0};
             hashmap_iter_init(&iter, tmp2);
             do
