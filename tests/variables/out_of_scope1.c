a80c2dff7e8dbb8b899af3231974a9679a51f593
Siirko <alexander.yanovskyy@gmail.com>
2024-01-12 22:37:47 +0100

    added test for out of scope

---
+++ tests/variables/out_of_scope1.c
@@ -0,0 +1,17 @@
+int main()
+{
+    int a;
+    j = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        a = 1;
+        for (int j = 0; j < 10; j++)
+        {
+            a = 1;
+        }
+        j = 0;
+    }
+    i = 0;
+    j = 0;
+    return 0;
+}
